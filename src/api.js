beb9d74fa720c449bab1ccb95794e82a9aca705a
Paul.Inglsmit <paul.inglsmit@gmail.com>
2022-12-14 23:23:03 +0300

    UPD: refactoring: part 4 - API rebuild to WebSocket

--- src/api.js
+++ src/api.js
@@ -1,38 +1,64 @@
-const API_KEY = "ce3fd966e7a1d10d65f907b20bf000552158fd3ed1bd614110baa0ac6cb57a7e";
-const tickersHandlers = new Map();
+const API_KEY = "380ec498044c900f249ad39326e8320a2cb4ee09b94afe4dff6911e37ef56bfc";
 
-const loadTickers = () => {
-    if(tickersHandlers.size === 0) return;
+const tickersHandlers = new Map(); // {}
+const socket = new WebSocket(
+    `wss://streamer.cryptocompare.com/v2?api_key=${API_KEY}`
+);
 
-    fetch(
-        `https://min-api.cryptocompare.com/data/pricemulti?fsyms=${[...tickersHandlers.keys()].join(',')}&tsyms=USD&api_key=${API_KEY}`
-    )
-        .then(res => res.json())
-        .then(rawData => {
-            const updatedPrices = Object.fromEntries(
-                Object.entries(rawData).map(([key, value]) => [key, value.USD])
-            );
+const AGGREGATE_INDEX = "5";
 
-            Object.entries(updatedPrices).forEach(([currency, newPrice]) => {
-                const handlers = tickersHandlers.get(currency) ?? [];
-                handlers.forEach((fn) => fn(newPrice));
-            })
+socket.addEventListener("message", e => {
+    const { TYPE: type, FROMSYMBOL: currency, PRICE: newPrice } = JSON.parse(
+        e.data
+    );
+    if (type !== AGGREGATE_INDEX || newPrice === undefined) {
+        return;
+    }
 
-        });
+    const handlers = tickersHandlers.get(currency) ?? [];
+    handlers.forEach(fn => fn(newPrice));
+});
+
+function sendToWebSocket(message) {
+    const stringifiedMessage = JSON.stringify(message);
+
+    if (socket.readyState === WebSocket.OPEN) {
+        socket.send(stringifiedMessage);
+        return;
+    }
+
+    socket.addEventListener(
+        "open",
+        () => {
+            socket.send(stringifiedMessage);
+        },
+        { once: true }
+    );
+}
+
+function subscribeToTickerOnWs(ticker) {
+    sendToWebSocket({
+        action: "SubAdd",
+        subs: [`5~CCCAGG~${ticker}~USD`]
+    });
+}
+
+function unsubscribeFromTickerOnWs(ticker) {
+    sendToWebSocket({
+        action: "SubRemove",
+        subs: [`5~CCCAGG~${ticker}~USD`]
+    });
 }
 
 // Inside this Map object - functions list - which we call when some ticker will update
 export const subscribeToTicker = (ticker, cb) => {
     const subscribers = tickersHandlers.get(ticker) || [];
     tickersHandlers.set(ticker, [...subscribers, cb]);
+    subscribeToTickerOnWs(ticker);
 };
 
 // We left only fn's which is not equal of current cb
-export const unsubscribeFromTicker = (ticker, cb) => {
-    // const subscribers = tickersHandlers.get(ticker) || [];
-    // tickersHandlers.set(ticker, subscribers.filter(fn => fn !== cb));
-    tickersHandlers.delete(ticker)
+export const unsubscribeFromTicker = ticker => {
+    tickersHandlers.delete(ticker);
+    unsubscribeFromTickerOnWs(ticker);
 };
-
-setInterval(loadTickers, 5000);
-window.tickers = tickersHandlers;
